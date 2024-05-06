39db9a3583f8a621a83054b84eac1a134dd8fec6
assilbekov <askhat.assilbekov@gmail.com>
2024-05-06 14:24:07 -0300

    implement subs modek

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package observer
+package main
+
+import "observer/pkg"
+
+func main() {
+	sub1 := &pkg.Subscriber{Name: "sub1"}
+	sub2 := &pkg.Subscriber{Name: "sub2"}
+	sub3 := &pkg.Subscriber{Name: "sub3"}
+	channel := pkg.Publisher{
+		Name: "Channel",
+		Consumers: map[string]pkg.Consumer{
+			sub1.GetName(): sub1,
+		},
+	}
+	channel.Subscribe(sub3)
+	channel.Subscribe(sub2)
+
+	channel.Notify()
+
+	channel.Unsubscribe(sub2)
+
+	channel.Notify()
+}
--- pkg/publisher.go
+++ pkg/publisher.go
@@ -1,19 +1,24 @@
 package pkg
 
+import "fmt"
+
 type Publisher struct {
 	Name      string
 	Consumers map[string]Consumer
 }
 
 func (p *Publisher) Subscribe(c Consumer) {
+	fmt.Println("Subscribed", c.GetName(), "to", p.Name)
 	p.Consumers[c.GetName()] = c
 }
 
 func (p *Publisher) Unsubscribe(c Consumer) {
+	fmt.Println("Unsubscribed", c.GetName(), "from", p.Name)
 	delete(p.Consumers, c.GetName())
 }
 
 func (p *Publisher) Notify() {
+	fmt.Println("Notifying consumers of", p.Name)
 	for _, c := range p.Consumers {
 		c.Update(p.Name)
 	}
